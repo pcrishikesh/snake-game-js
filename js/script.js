dda81e7a15297531231f4ca7b36c96ce689ddd24
Rishikesh pc <hunterpckgh@gmail.com>
2023-06-09 23:27:13 +0530

    mad game window and grid for moving snake

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-alert('hello')
+// variable declaration
+
+let cvs = document.getElementById('canvas').getContext("2d")
+
+// onload window function
+
+window.onload = ()=>{
+    mainGame()
+}
+
+// main game function
+
+function mainGame() {
+
+    // game area
+    
+    // canvas background color
+
+    cvs.fillStyle = "green"
+    cvs.fillRect(0,0,400,400)
+
+    // grid line
+
+    for (cl=0;cl<400;cl += 20) {
+        cvs.moveTo(cl,0)
+        cvs.lineTo(cl, 400)
+    }
+
+    for (rl=0;rl<400;rl += 20) {
+        cvs.moveTo(0,rl)
+        cvs.lineTo(400, rl)
+    }
+
+    cvs.stroke()
+
+}
+
+// input controll function
